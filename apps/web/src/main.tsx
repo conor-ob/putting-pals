3772dc6da778c7ff03bf31704a671256b880f7fc
conor-ob <conor.w.ob@gmail.com>
2024-11-21 00:12:14 +0000

    debug service worker

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,11 +3,12 @@ import "./instrument";
 import React from "react";
 import * as Sentry from "@sentry/react";
 import { createRoot } from "react-dom/client";
-import { registerSW } from "virtual:pwa-register";
+
+// import { registerSW } from "virtual:pwa-register";
 
 import App from "./App";
 
-registerSW({ immediate: true });
+// registerSW({ immediate: true });
 
 const container = document.getElementById("root");
 if (!container) {
