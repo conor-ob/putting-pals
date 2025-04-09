e662a8c7e83b63530b113b4ec5bf2a4540c494a7
conor-ob <conor.w.ob@gmail.com>
2025-04-09 21:42:59 +0100

    styles

--- apps/web/src/features/competition/leaderboard/table.tsx
+++ apps/web/src/features/competition/leaderboard/table.tsx
@@ -3,39 +3,64 @@ import { LeadboardPlayerRow } from "@features/leaderboard/table/player-row";
 import { api } from "@providers/trpc-provider";
 
 import { cn } from "@pkg/ui/cn";
+import { Skeleton } from "@pkg/ui/ui";
 
 export function CompetitionTable({ id }: { id?: string }) {
   const { data } = api.competition.getById.useQuery({ id });
 
+  if (data === undefined) {
+    return (
+      <div className="flex flex-col px-4">
+        <div className="border-b py-4">
+          <Skeleton className="h-3 w-full" />
+        </div>
+        <div className="border-b py-4">
+          <Skeleton className="h-3 w-full" />
+        </div>
+        <div className="border-b py-4">
+          <Skeleton className="h-3 w-full" />
+        </div>
+        <div className="border-b py-4">
+          <Skeleton className="h-3 w-full" />
+        </div>
+        <div className="border-b py-4">
+          <Skeleton className="h-3 w-full" />
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
-      {data?.competitors.map((competitor) => {
+      {data.competitors.map((competitor) => {
         return (
           <div key={competitor.id}>
             <ListItem>
-              <div className="flex w-full flex-row justify-between px-4 py-3">
-                <div className="flex flex-row items-center">
-                  <div className="w-10 text-sm font-semibold tracking-tighter">
-                    {competitor.position}
+              <div className="flex w-full flex-col">
+                <div className="flex w-full flex-row justify-between px-4 py-3">
+                  <div className="flex flex-row items-center">
+                    <div className="w-10 text-sm font-semibold tracking-tighter">
+                      {competitor.position}
+                    </div>
+                    <div className="me-2 w-8 px-0.5"></div>
+                    <div className="line-clamp-1 text-sm font-semibold tracking-tighter">
+                      {competitor.shortName}
+                    </div>
                   </div>
-                  <div className="me-2 w-8 px-0.5"></div>
-                  <div className="line-clamp-1 text-sm font-semibold tracking-tighter">
-                    {competitor.shortName}
+                  <div className="flex flex-row">
+                    <div
+                      className={cn(
+                        "flex w-12 justify-center text-sm font-semibold tracking-tighter",
+                        competitor.totalSort < 0 && "text-red",
+                        competitor.totalSort === 0 && "text-green",
+                      )}
+                    >
+                      {competitor.total}
+                    </div>
+                    <div className="flex w-10 justify-center text-sm font-semibold tracking-tighter"></div>
+                    <div className="flex w-8 justify-end text-sm font-semibold tracking-tighter"></div>
                   </div>
                 </div>
-                <div className="flex flex-row">
-                  <div
-                    className={cn(
-                      "flex w-12 justify-center text-sm font-semibold tracking-tighter",
-                      competitor.totalSort < 0 && "text-red",
-                      competitor.totalSort === 0 && "text-green",
-                    )}
-                  >
-                    {competitor.total}
-                  </div>
-                  <div className="flex w-10 justify-center text-sm font-semibold tracking-tighter"></div>
-                  <div className="flex w-8 justify-end text-sm font-semibold tracking-tighter"></div>
-                </div>
+                <div className="mx-4 h-0.5 bg-foreground"></div>
               </div>
             </ListItem>
             {competitor.picks.map((player) => {
@@ -52,6 +77,7 @@ export function CompetitionTable({ id }: { id?: string }) {
                     score={player.scoringData.score}
                     teeTime={player.scoringData.teeTime}
                   />
+                  <div className="mx-4 border-b"></div>
                 </div>
               );
             })}
