3fb6848b0ae5434166cf7d239525505c19306d60
conor-ob <conor.w.ob@gmail.com>
2025-04-10 13:19:54 +0100

    favs

--- apps/web/src/features/competition/leaderboard/table.tsx
+++ apps/web/src/features/competition/leaderboard/table.tsx
@@ -24,7 +24,7 @@ export function CompetitionTable({
   const [favourites, setFavourites] = useState<string[]>([]);
 
   // TODO remove hardcoded key
-  const cacheKey = favouritesStorageKey('"R2025014"');
+  const cacheKey = favouritesStorageKey("R2025014");
   useEffect(() => {
     function loadPicks() {
       try {
