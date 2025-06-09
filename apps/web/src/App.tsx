1e979d13b6e9a6e80e6c16943e07163c0152d234
conor-ob <conor.w.ob@gmail.com>
2025-06-09 21:45:03 +0100

    odds route for all

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,20 +1,6 @@
-import { CompetitionPage } from "@features/competition/page";
-import { LeaderboardPage } from "@features/leaderboard/page";
 import { OddsPage } from "@features/odds/page";
-import {
-  IonApp,
-  // IonIcon,
-  // IonLabel,
-  IonRouterOutlet,
-  // IonTabBar,
-  // IonTabButton,
-  IonTabs,
-  setupIonicReact,
-} from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
+import { IonApp, setupIonicReact } from "@ionic/react";
 import { TrpcProvider } from "@providers/trpc-provider";
-// import { square, trophy } from "ionicons/icons";
-import { Redirect, Route } from "react-router-dom";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -53,6 +39,55 @@ const App: React.FC = () => (
   </IonApp>
 );
 
+export default App;
+
+// import { CompetitionPage } from "@features/competition/page";
+// import { LeaderboardPage } from "@features/leaderboard/page";
+// import { OddsPage } from "@features/odds/page";
+// import {
+//   IonApp,
+//   // IonIcon,
+//   // IonLabel,
+//   IonRouterOutlet,
+//   // IonTabBar,
+//   // IonTabButton,
+//   IonTabs,
+//   setupIonicReact,
+// } from "@ionic/react";
+// import { IonReactRouter } from "@ionic/react-router";
+// import { TrpcProvider } from "@providers/trpc-provider";
+// // import { square, trophy } from "ionicons/icons";
+// import { Redirect, Route } from "react-router-dom";
+
+// /* Core CSS required for Ionic components to work properly */
+// import "@ionic/react/css/core.css";
+// /* Basic CSS for apps built with Ionic */
+// import "@ionic/react/css/normalize.css";
+// import "@ionic/react/css/structure.css";
+// import "@ionic/react/css/typography.css";
+// /* Optional CSS utils that can be commented out */
+// import "@ionic/react/css/padding.css";
+// import "@ionic/react/css/float-elements.css";
+// import "@ionic/react/css/text-alignment.css";
+// import "@ionic/react/css/text-transformation.css";
+// import "@ionic/react/css/flex-utils.css";
+// import "@ionic/react/css/display.css";
+// /**
+//  * Ionic Dark Mode
+//  * -----------------------------------------------------
+//  * For more info, please see:
+//  * https://ionicframework.com/docs/theming/dark-mode
+//  */
+// /* import '@ionic/react/css/palettes/dark.always.css'; */
+// // import "@ionic/react/css/palettes/dark.class.css";
+// import "@ionic/react/css/palettes/dark.system.css";
+// /* Theme variables */
+// import "@theme/variables.css";
+// // UI variables
+// import "@pkg/ui/globals.css";
+
+// setupIonicReact({ mode: "ios" });
+
 // const App: React.FC = () => (
 //   <IonApp>
 //     <TrpcProvider>
@@ -88,4 +123,4 @@ const App: React.FC = () => (
 //   </IonApp>
 // );
 
-export default App;
+// export default App;
