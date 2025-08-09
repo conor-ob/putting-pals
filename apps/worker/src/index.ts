895abd0eda035e118b4e063d20275a820ac5707a
conor-ob <conor.w.ob@gmail.com>
2025-08-09 17:03:05 +0100

    sched

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -38,7 +38,70 @@ export default {
 	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
 	// [[triggers]] configuration.
 	async scheduled(event, env, ctx): Promise<void> {
-		handleRequest(env);
+		const leaderboardId = 'R2025027';
+		const leaderboardClient = new LeaderboardClient();
+		const leaderboard = await leaderboardClient.getLeaderboard(leaderboardId);
+		const players = leaderboard.players
+			.slice(0, 39)
+			.filter((row) => row.__typename === 'PlayerRowV3')
+			.map((row) => {
+				const playerRow = row as PlayerRowV3;
+				return {
+					player: {
+						name: playerRow.player.displayName,
+					},
+					scoringData: {
+						position: playerRow.scoringData.position,
+						totalScore: playerRow.scoringData.total,
+						// todayScore: playerRow.scoringData.score,
+						// holesPlayed: playerRow.scoringData.thru,
+					},
+				};
+			});
+
+		const { results } = await env.DB.prepare('SELECT * FROM leaderboard_v3 WHERE id = ?').bind(leaderboardId).run();
+
+		if (results.length === 0) {
+			await env.DB.prepare('INSERT INTO leaderboard_v3 (id, data) VALUES (?1, ?2)').bind(leaderboardId, JSON.stringify(players)).run();
+		} else if (results.length === 1) {
+			const leaderboardData = JSON.stringify(players);
+			const rowData = results[0].data;
+			if (leaderboardData !== rowData) {
+				console.log('leaderboard changed');
+				await env.DB.prepare('UPDATE leaderboard_v3 SET data = ?1 WHERE id = ?2').bind(JSON.stringify(players), leaderboardId).run();
+
+				const response = await env.AI.run(
+					'@cf/meta/llama-4-scout-17b-16e-instruct',
+					{
+						messages: [
+							{
+								role: 'system',
+								content:
+									'You are a charismatic, funny and well informed golf commentator. You will receive 2 leaderboard JSON objects and analyse the differences between them. Respond with the most significant difference. Do not use filler words. Imagine you are commentating to a real audience. Reply with 1 or 2 sentences. Use golf language. Do not use filler like "The most significant difference is ..." or "The only change is ..." just say what has changed and NOTHING ELSE."',
+							},
+							{
+								role: 'user',
+								content: JSON.stringify({ leaderboardBefore: rowData, leaderboardAfter: leaderboardData }),
+							},
+						],
+						max_tokens: 1024,
+					},
+					{
+						returnRawResponse: true,
+					},
+				);
+
+				const json = await response.json();
+
+				const aiResponse = (json as { response: string }).response;
+
+				await env.DB.prepare('INSERT INTO feed (post) VALUES (?)').bind(aiResponse).run();
+			} else {
+				console.log('leaderboard did not change');
+			}
+		} else {
+			console.log('unexpected state');
+		}
 	},
 } satisfies ExportedHandler<Env>;
 
