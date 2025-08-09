25e4f3d1e5a916868c3ddc6d8ace7193bd2d1229
conor-ob <conor.w.ob@gmail.com>
2025-08-09 13:23:56 +0100

    basic worker

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,4 +1,17 @@
 /**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+/**
  * Welcome to Cloudflare Workers!
  *
  * This is a template for a Scheduled Worker: a Worker that can run on a
@@ -14,22 +27,14 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-import { LeaderboardClient } from '@pkg/graphql/leaderboard';
-
 export default {
-	async fetch(req) {
-		const leaderboardClient = new LeaderboardClient();
-		const leaderboard = await leaderboardClient.getLeaderboard('R2025027');
-		return Response.json(leaderboard);
+	async fetch(request, env, ctx): Promise<Response> {
+		return new Response('Hello World!');
 	},
 
 	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
 	// [[triggers]] configuration.
 	async scheduled(event, env, ctx): Promise<void> {
-		const leaderboardClient = new LeaderboardClient();
-		const leaderboard = await leaderboardClient.getLeaderboard('R2025027');
-		console.log(leaderboard);
-
-		console.log(`trigger fired at ${event.cron}: ${leaderboard.id}`);
+		console.log(`trigger fired at ${event.cron}`);
 	},
 } satisfies ExportedHandler<Env>;
