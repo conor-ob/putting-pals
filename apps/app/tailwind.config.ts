44c1ac658991ed8de209a87086447fbb21512cbc
conor-ob <conor.w.ob@gmail.com>
2025-11-15 11:58:51 +0000

    dark mode

--- apps/app/tailwind.config.ts
+++ apps/app/tailwind.config.ts
@@ -5,7 +5,8 @@ import nativewind from "nativewind/preset";
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   presets: [nativewind],
-  darkMode: "media",
+  // darkMode: "media", // FIXME: Uncaught Error: Cannot manually set color scheme, as dark mode is type 'media'. Please use StyleSheet.setFlag('darkMode', 'class')
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
