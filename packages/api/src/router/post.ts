0a88e2feb09f0bcb3f9f8a02e798f7179a5fb52a
conor-ob <conor.w.ob@gmail.com>
2025-11-16 17:05:42 +0000

    remove unused apps and packages

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,34 +1,44 @@
 import type { TRPCRouterRecord } from "@trpc/server";
 import { z } from "zod/v4";
 
-import { desc, eq } from "@acme/db";
-import { CreatePostSchema, Post } from "@acme/db/schema";
+import { publicProcedure } from "../trpc";
 
-import { protectedProcedure, publicProcedure } from "../trpc";
+const posts = [
+  {
+    id: "1",
+    title: "Hello, world!",
+    content: "This is a test post",
+  },
+  {
+    id: "2",
+    title: "Hello, world!",
+    content: "This is a test post",
+  },
+  {
+    id: "3",
+    title: "Hello, world!",
+    content: "This is a test post",
+  },
+]
 
 export const postRouter = {
   all: publicProcedure.query(({ ctx }) => {
-    return ctx.db.query.Post.findMany({
-      orderBy: desc(Post.id),
-      limit: 10,
-    });
+    return posts
   }),
 
   byId: publicProcedure
     .input(z.object({ id: z.string() }))
     .query(({ ctx, input }) => {
-      return ctx.db.query.Post.findFirst({
-        where: eq(Post.id, input.id),
-      });
+        return posts.find((post) => post.id === input.id);
     }),
 
-  create: protectedProcedure
-    .input(CreatePostSchema)
+  create: publicProcedure
+    .input(z.object({ title: z.string(), content: z.string() }))
     .mutation(({ ctx, input }) => {
-      return ctx.db.insert(Post).values(input);
+      return posts.push({ id: crypto.randomUUID(), ...input });
     }),
 
-  delete: protectedProcedure.input(z.string()).mutation(({ ctx, input }) => {
-    return ctx.db.delete(Post).where(eq(Post.id, input));
+  delete: publicProcedure.input(z.string()).mutation(({ ctx, input }) => {
+    return posts.filter((post) => post.id !== input);
   }),
 } satisfies TRPCRouterRecord;
--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -6,12 +6,9 @@
  * tl;dr - this is where all the tRPC server stuff is created and plugged in.
  * The pieces you will need to use are documented accordingly near the end
  */
-import { initTRPC, TRPCError } from "@trpc/server";
+import { initTRPC } from "@trpc/server";
 import superjson from "superjson";
 import { z, ZodError } from "zod/v4";
-
-import type { Auth } from "@acme/auth";
-import { db } from "@acme/db/client";
 
 /**
  * 1. CONTEXT
@@ -26,19 +23,8 @@ import { db } from "@acme/db/client";
  * @see https://trpc.io/docs/server/context
  */
 
-export const createTRPCContext = async (opts: {
-  headers: Headers;
-  auth: Auth;
-}) => {
-  const authApi = opts.auth.api;
-  const session = await authApi.getSession({
-    headers: opts.headers,
-  });
-  return {
-    authApi,
-    session,
-    db,
-  };
+export const createTRPCContext = async (opts: { headers: Headers }) => {
+  return {};
 };
 /**
  * 2. INITIALIZATION
@@ -104,25 +90,3 @@ const timingMiddleware = t.middleware(async ({ next, path }) => {
  * can still access user session data if they are logged in
  */
 export const publicProcedure = t.procedure.use(timingMiddleware);
-
-/**
- * Protected (authenticated) procedure
- *
- * If you want a query or mutation to ONLY be accessible to logged in users, use this. It verifies
- * the session is valid and guarantees `ctx.session.user` is not null.
- *
- * @see https://trpc.io/docs/procedures
- */
-export const protectedProcedure = t.procedure
-  .use(timingMiddleware)
-  .use(({ ctx, next }) => {
-    if (!ctx.session?.user) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return next({
-      ctx: {
-        // infers the `session` as non-nullable
-        session: { ...ctx.session, user: ctx.session.user },
-      },
-    });
-  });
