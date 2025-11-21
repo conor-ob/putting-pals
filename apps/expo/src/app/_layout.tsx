4985c307f0f0b8190cbdefe4e311c891a465f07a
conor-ob <conor.w.ob@gmail.com>
2025-11-21 20:07:50 +0000

    railway deployment

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,8 +1,8 @@
+import "@/global.css";
+
 import { ThemeProvider } from "@/providers/theme/theme-provider";
 import { TrpcProvider } from "@/providers/trpc/trpc-provider";
 import { Stack } from "expo-router";
-
-import "@/global.css";
 
 export default function Layout() {
   return (
