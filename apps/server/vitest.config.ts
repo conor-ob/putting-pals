72db2844b189bb07437cf1bfc0ddc8d47fcf2d1f
conor-ob <conor.w.ob@gmail.com>
2025-11-22 09:49:50 +0000

    format

--- apps/server/vitest.config.ts
+++ apps/server/vitest.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
-import { configDefaults, defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
