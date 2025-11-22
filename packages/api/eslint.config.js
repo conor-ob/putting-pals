edeab2b82542895c8f212813f1978ca2e077ada2
conor-ob <conor.w.ob@gmail.com>
2025-11-22 10:40:55 +0000

    remove eslint and prettier

--- packages/api/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from "@putting-pals/eslint/base";
-
-/** @type {import('typescript-eslint').Config} */
-export default [
-  {
-    ignores: ["dist/**"],
-  },
-  ...baseConfig,
-];
