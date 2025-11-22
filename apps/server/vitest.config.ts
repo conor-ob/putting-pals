681e00659c9caf2be3af06af784d0e98c0afebe9
conor-ob <conor.w.ob@gmail.com>
2025-11-22 09:47:15 +0000

    ts config paths

--- apps/server/vitest.config.ts
+++ apps/server/vitest.config.ts
@@ -1,7 +1,9 @@
 /// <reference types="vitest" />
 import { configDefaults, defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     exclude: [...configDefaults.exclude],
     reporters: "verbose",
