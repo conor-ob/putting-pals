8bc2c8fa7cd033abafa2cdaacc00edaa3304fcc6
conor-ob <conor.w.ob@gmail.com>
2025-11-22 17:01:13 +0000

    bugfix(api): fix trpc base url

--- apps/expo/src/providers/trpc/utils/trpc.ts
+++ apps/expo/src/providers/trpc/utils/trpc.ts
@@ -2,6 +2,7 @@ import type { AppRouter } from "@putting-pals/api/router";
 import { QueryClient } from "@tanstack/react-query";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import { Platform } from "react-native";
 import superjson from "superjson";
 
 export const queryClient = new QueryClient();
@@ -9,10 +10,13 @@ export const queryClient = new QueryClient();
 const trpcClient = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url:
-        process.env.NODE_ENV === "development"
-          ? "http://localhost:4000/trpc"
-          : "https://puttingpals.up.railway.app/api/trpc",
+      url: Platform.select({
+        web: process.env.NODE_ENV === "production" ? "api/trpc" : "http://localhost:4000/trpc",
+        default:
+          process.env.NODE_ENV === "production"
+            ? "https://puttingpals.up.railway.app/api/trpc"
+            : "http://localhost:4000/trpc", // TODO: needs IP address
+      }),
       transformer: superjson,
     }),
   ],
