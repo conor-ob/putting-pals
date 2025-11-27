c87a0da434e66044be24542c0d99e4825d71d940
conor-ob <conor.w.ob@gmail.com>
2025-11-27 20:58:11 +0000

    render leaderboard

--- apps/expo/src/features/home/home-page.tsx
+++ apps/expo/src/features/home/home-page.tsx
@@ -5,16 +5,17 @@ import { Text } from "~/components/ui/text";
 import { trpc } from "~/providers/trpc/utils/trpc";
 
 export function HomePage() {
+  const tourCode = "P";
   const { data: tournament } = useQuery(
-    trpc.tournament.getById.queryOptions({ tourCode: "P" }),
+    trpc.tournament.getById.queryOptions({ tourCode }),
   );
 
   const { data: leaderboard } = useQuery(
-    trpc.leaderboard.getById.queryOptions({ tourCode: "P" }),
+    trpc.leaderboard.getById.queryOptions({ tourCode }),
   );
 
   const { data: schedule } = useQuery(
-    trpc.schedule.get.queryOptions({ tourCode: "P" }),
+    trpc.schedule.get.queryOptions({ tourCode }),
   );
 
   // biome-ignore lint/suspicious/noConsole: testing
@@ -25,13 +26,27 @@ export function HomePage() {
   console.log("schedule", schedule);
 
   return (
-    <ScrollView className="">
-      {tournament && (
-        <TournamentHeader tournament={tournament} className="p-4" />
-      )}
+    <ScrollView className="p-4">
+      {tournament && <TournamentHeader tournament={tournament} />}
       {leaderboard?.rows.map((row) => (
-        <View key={row.id}>
-          <Text>{row.__typename}</Text>
+        <View key={row.id} className="flex flex-row items-center gap-2">
+          {row.__typename === "PlayerRowV3" ? (
+            <View className="flex flex-row w-full items-center gap-2">
+              <Text className="w-1/3">{row.player.displayName}</Text>
+              <Text className="w-1/3">{row.scoringData.position}</Text>
+              <Text className="w-1/3">{row.scoringData.total}</Text>
+            </View>
+          ) : row.__typename === "PuttingPalsPlayerRow" ? (
+            <View className="flex flex-row w-full items-center gap-2">
+              <Text className="w-1/3">{row.player.displayName}</Text>
+              <Text className="w-1/3">{row.scoringData.position}</Text>
+              <Text className="w-1/3">{row.scoringData.total}</Text>
+            </View>
+          ) : row.__typename === "InformationRow" ? (
+            <View className="flex flex-row w-full items-center gap-2">
+              <Text>{row.displayText}</Text>
+            </View>
+          ) : null}
         </View>
       ))}
     </ScrollView>
