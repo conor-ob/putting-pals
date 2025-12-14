30338877fa413cb2aff5d9de626d4974b632fc89
conor-ob <conor.w.ob@gmail.com>
2025-12-14 10:43:59 +0000

    add scheduled worker

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,8 +1,13 @@
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
+ * This is a template for a Scheduled Worker: a Worker that can run on a
+ * configurable interval:
+ * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
+ *
  * - Run `npm run dev` in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"` to see your Worker in action
  * - Run `npm run deploy` to publish your worker
  *
  * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
@@ -12,7 +17,27 @@
  */
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env, ctx): Promise<Response> {
+    const url = new URL(request.url);
+    url.pathname = "/__scheduled";
+    url.searchParams.append("cron", "* * * * *");
+    return new Response(
+      `To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`,
+    );
+  },
+
+  // The scheduled handler is invoked at the interval set in our wrangler.jsonc's
+  // [[triggers]] configuration.
+  async scheduled(event, env, ctx): Promise<void> {
+    // A Cron Trigger can make requests to other endpoints on the Internet,
+    // publish to a Queue, query a D1 Database, and much more.
+    //
+    // We'll keep it simple and make an API call to a Cloudflare API:
+    const resp = await fetch("https://api.cloudflare.com/client/v4/ips");
+    const wasSuccessful = resp.ok ? "success" : "fail";
+
+    // You could store this result in KV, write to a D1 Database, or publish to a Queue.
+    // In this template, we'll just log the result:
+    console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+  },
 } satisfies ExportedHandler<Env>;
