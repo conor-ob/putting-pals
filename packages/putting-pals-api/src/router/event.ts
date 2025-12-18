20d37785efed8f339250995b43e9388f423c8063
conor-ob <conor.w.ob@gmail.com>
2025-12-18 15:38:30 +0000

    bump v

--- packages/putting-pals-api/src/router/event.ts
+++ packages/putting-pals-api/src/router/event.ts
@@ -13,7 +13,7 @@ export const eventRouter = router({
       // biome-ignore lint/suspicious/noConsole: testing
       console.log("processing event", input.cron, input.scheduledTime);
       return {
-        message: "test 12",
+        message: "test 13",
         cron: input.cron,
         scheduledTime: input.scheduledTime,
         timestamp: Date.now(),
