537547439685fd1125def39c3387607ea8736c96
conor-ob <conor.w.ob@gmail.com>
2025-12-18 23:28:41 +0000

    test

--- packages/putting-pals-db/src/env/schema.ts
+++ packages/putting-pals-db/src/env/schema.ts
@@ -8,7 +8,9 @@ dotenv({
 });
 
 const envSchema = z.object({
-  DATABASE_URL: z.url(),
+  DATABASE_URL: z
+    .url()
+    .default("postgresql://postgres:postgres@localhost:5432/putting-pals"), // TODO: change to use .env file
 });
 
 export const env = envSchema.parse(process.env);
