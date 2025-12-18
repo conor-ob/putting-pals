82fc434ac1dead07cfdeee4e5124e4ec660dd79f
conor-ob <conor.w.ob@gmail.com>
2025-12-18 23:23:17 +0000

    feat(db): add postgress db

--- packages/putting-pals-api/src/router/event.ts
+++ packages/putting-pals-api/src/router/event.ts
@@ -1,5 +1,19 @@
+import { LeaderboardService } from "@putting-pals/putting-pals-core/leaderboard";
+import { TournamentService } from "@putting-pals/putting-pals-core/tournament";
+import type { db } from "@putting-pals/putting-pals-db/client";
+import {
+  type LeaderboardSnapshotV1,
+  leaderboardSnapshotBaseTable,
+  leaderboardSnapshotPatchTable,
+} from "@putting-pals/putting-pals-db/schema";
+import { asc, desc, eq } from "drizzle-orm";
+import patch from "fast-json-patch";
 import z from "zod";
 import { publicProcedure, router } from "../trpc";
+
+type Ctx = {
+  db: typeof db;
+};
 
 export const eventRouter = router({
   processEvent: publicProcedure
@@ -9,14 +23,79 @@ export const eventRouter = router({
         scheduledTime: z.number(),
       }),
     )
-    .mutation(async ({ input }) => {
+    .mutation(async ({ ctx, input }) => {
       // biome-ignore lint/suspicious/noConsole: testing
       console.log("processing event", input.cron, input.scheduledTime);
-      return {
-        message: "OK",
-        cron: input.cron,
-        scheduledTime: input.scheduledTime,
-        timestamp: Date.now(),
-      };
+      return processEvent(ctx);
     }),
 });
+
+async function processEvent(ctx: Ctx) {
+  const tourCode = "S";
+  const tournamentId = "S2025600";
+
+  const [tournament, leaderboard] = await Promise.all([
+    new TournamentService().getTournament("R", tournamentId),
+    new LeaderboardService().getLeaderboard("R", tournamentId),
+  ]);
+
+  const newSnapshot = {
+    __typename: "LeaderboardSnapshotV1" as const,
+    ...tournament,
+    ...leaderboard,
+  } satisfies LeaderboardSnapshotV1; // required for stricter type checking
+
+  const db = ctx.db;
+
+  const base = await db
+    .select()
+    .from(leaderboardSnapshotBaseTable)
+    .where(eq(leaderboardSnapshotBaseTable.tournamentId, tournamentId))
+    .orderBy(desc(leaderboardSnapshotBaseTable.createdAt))
+    .limit(1);
+
+  const baseSnapshot = base[0]?.snapshot;
+
+  if (baseSnapshot === undefined || baseSnapshot === null) {
+    await db.insert(leaderboardSnapshotBaseTable).values({
+      tournamentId: tournamentId,
+      tourCode: tourCode,
+      snapshot: newSnapshot,
+    });
+    return;
+  }
+
+  const patches = await db
+    .select()
+    .from(leaderboardSnapshotPatchTable)
+    .where(eq(leaderboardSnapshotPatchTable.tournamentId, tournamentId))
+    .orderBy(asc(leaderboardSnapshotPatchTable.createdAt));
+
+  const materialized = patch.applyPatch(
+    structuredClone(baseSnapshot),
+    patches.flatMap((patch) => patch.patch.operations),
+    false,
+  ).newDocument;
+
+  const diff = patch.compare(materialized, newSnapshot);
+  if (diff.length > 0) {
+    const lastStreamVersion = await db
+      .select()
+      .from(leaderboardSnapshotPatchTable)
+      .where(eq(leaderboardSnapshotPatchTable.tournamentId, tournamentId))
+      .orderBy(desc(leaderboardSnapshotPatchTable.streamVersion))
+      .limit(1);
+
+    const streamVersion = lastStreamVersion[0]?.streamVersion ?? 0;
+
+    await db.insert(leaderboardSnapshotPatchTable).values({
+      tournamentId: tournamentId,
+      tourCode: tourCode,
+      patch: {
+        __typename: "JsonPatchV1" as const,
+        operations: diff,
+      },
+      streamVersion: streamVersion + 1,
+    });
+  }
+}
--- packages/putting-pals-db/src/env/schema.ts
+++ packages/putting-pals-db/src/env/schema.ts
@@ -8,20 +8,7 @@ dotenv({
 });
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(["production", "development", "test"])
-    .default("development"),
-  PORT: z
-    .string()
-    .transform((s) => parseInt(s, 10))
-    .pipe(z.number()),
-  ORIGIN: z.url(),
   DATABASE_URL: z.url(),
-  CI: z
-    .string()
-    .default("false")
-    .transform((s) => s === "true")
-    .pipe(z.boolean()),
 });
 
 export const env = envSchema.parse(process.env);
