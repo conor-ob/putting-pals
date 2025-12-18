3a1d0a74d53584128ce70cbd58f9fdf0ffdc32e9
conor-ob <conor.w.ob@gmail.com>
2025-12-18 20:05:08 +0000

    updates

--- apps/server/src/env/schema.ts
+++ apps/server/src/env/schema.ts
@@ -1,12 +1,11 @@
-import "dotenv/config";
-// import { config as dotenv } from "dotenv";
+import { config as dotenv } from "dotenv";
 import { z } from "zod";
 
-// dotenv({
-//   encoding: "utf8",
-//   quiet: process.env.NODE_ENV === "production",
-//   debug: process.env.NODE_ENV !== "production",
-// });
+dotenv({
+  encoding: "utf8",
+  quiet: process.env.NODE_ENV === "production",
+  debug: process.env.NODE_ENV !== "production",
+});
 
 const envSchema = z.object({
   NODE_ENV: z
