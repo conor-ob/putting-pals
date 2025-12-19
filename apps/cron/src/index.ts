ab8eb8bfd77bda1e7718decc03d084ff575fbd1d
Conor O'Brien <conor.w.ob@gmail.com>
2025-12-19 22:00:23 +0000

    feat(cron): railway cron worker (#60)

---
+++ apps/cron/src/index.ts
@@ -0,0 +1,27 @@
+import type { AppRouter } from "@putting-pals/putting-pals-api/router";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import superjson from "superjson";
+import { env } from "./env/schema";
+
+async function sendEvent() {
+  // biome-ignore lint/suspicious/noConsole: debugging
+  console.log("env.TRPC_API_URL", env.TRPC_API_URL);
+  const client = createTRPCClient<AppRouter>({
+    links: [
+      httpBatchLink({
+        url: env.TRPC_API_URL,
+        transformer: superjson,
+      }),
+    ],
+  });
+
+  const response = await client.event.processEvent.mutate({
+    cron: "5 * * * *",
+    scheduledTime: Date.now(),
+  });
+
+  // biome-ignore lint/suspicious/noConsole: testing
+  console.log("processed event", response);
+}
+
+sendEvent();
