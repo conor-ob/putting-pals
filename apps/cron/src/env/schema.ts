3d5eda4ea1e40d24edea1206525a7b2aabcd36b9
conor-ob <conor.w.ob@gmail.com>
2025-12-19 21:07:17 +0000

    debug

--- apps/cron/src/env/schema.ts
+++ apps/cron/src/env/schema.ts
@@ -5,4 +5,7 @@ const envSchema = z.object({
   TRPC_API_URL: z.url(),
 });
 
+// biome-ignore lint/suspicious/noConsole: debugging
+console.log("process.env", process.env);
+
 export const env = envSchema.parse(process.env);
