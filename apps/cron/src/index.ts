0b5ef2f8aeea7c52df04cefeb677e02f9961d6c8
conor-ob <conor.w.ob@gmail.com>
2025-12-19 19:45:40 +0000

    remove env

--- apps/cron/src/index.ts
+++ apps/cron/src/index.ts
@@ -1,13 +1,12 @@
 import type { AppRouter } from "@putting-pals/putting-pals-api/router";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import superjson from "superjson";
-import { env } from "./env/schema";
 
 async function sendEvent() {
   const client = createTRPCClient<AppRouter>({
     links: [
       httpBatchLink({
-        url: env.TRPC_API_URL,
+        url: process.env.TRPC_API_URL ?? "http://localhost:4000/trpc",
         transformer: superjson,
       }),
     ],
