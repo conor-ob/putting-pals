ba21716ca7a4e6901028bcb2581ce8b6e7b93bde
conor-ob <conor.w.ob@gmail.com>
2025-12-29 16:56:18 +0000

    schema updates

--- packages/putting-pals-db/drizzle/0000_curly_kabuki.sql
+++ packages/putting-pals-db/drizzle/0000_curly_kabuki.sql
@@ -1,24 +1,24 @@
 CREATE TABLE "leaderboard_feed" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"tour_code" varchar NOT NULL,
-	"tournament_id" varchar NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	"deleted_at" timestamp,
+	"tour_code" text NOT NULL,
+	"tournament_id" text NOT NULL,
+	"created_at" timestamp (3) with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp (3) with time zone DEFAULT now() NOT NULL,
+	"deleted_at" timestamp (3) with time zone,
 	"seq" serial NOT NULL,
-	"type" varchar NOT NULL,
-	"feed_item" json NOT NULL
+	"type" text NOT NULL,
+	"feed_item" jsonb NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "leaderboard_snapshot" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"tour_code" varchar NOT NULL,
-	"tournament_id" varchar NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	"deleted_at" timestamp,
-	"type" varchar NOT NULL,
-	"snapshot" json NOT NULL
+	"tour_code" text NOT NULL,
+	"tournament_id" text NOT NULL,
+	"created_at" timestamp (3) with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp (3) with time zone DEFAULT now() NOT NULL,
+	"deleted_at" timestamp (3) with time zone,
+	"type" text NOT NULL,
+	"snapshot" jsonb NOT NULL
 );
 --> statement-breakpoint
 CREATE INDEX "leaderboard_feed_tournament_idx" ON "leaderboard_feed" USING btree ("tour_code","tournament_id");--> statement-breakpoint
--- packages/putting-pals-db/src/db/schema.ts
+++ packages/putting-pals-db/src/db/schema.ts
@@ -5,28 +5,47 @@ import type {
 } from "@putting-pals/pga-tour-schema/types";
 import {
   index,
-  json,
+  jsonb,
   pgTable,
   serial,
+  text,
   timestamp,
   uuid,
-  varchar,
 } from "drizzle-orm/pg-core";
+
+const defaultColumns = {
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    precision: 3,
+  })
+    .notNull()
+    .defaultNow(),
+  updatedAt: timestamp("updated_at", {
+    withTimezone: true,
+    precision: 3,
+  })
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  deletedAt: timestamp("deleted_at", {
+    withTimezone: true,
+    precision: 3,
+  }),
+};
+
+const tournamentIdentifierColumns = {
+  tourCode: text("tour_code", { enum: ["P", "R"] }).notNull(),
+  tournamentId: text("tournament_id").notNull(),
+};
 
 export const leaderboardSnapshotTable = pgTable(
   "leaderboard_snapshot",
   {
-    id: uuid("id").primaryKey().defaultRandom(),
-    tourCode: varchar("tour_code", { enum: ["P", "R"] }).notNull(),
-    tournamentId: varchar("tournament_id").notNull(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at")
-      .notNull()
-      .defaultNow()
-      .$onUpdate(() => new Date()),
-    deletedAt: timestamp("deleted_at"),
-    type: varchar("type").notNull(),
-    snapshot: json("snapshot").notNull().$type<LeaderboardSnapshotV1>(),
+    ...defaultColumns,
+    ...tournamentIdentifierColumns,
+    type: text("type").notNull(),
+    snapshot: jsonb("snapshot").notNull().$type<LeaderboardSnapshotV1>(),
   },
   (table) => [
     index("leaderboard_snapshot_tournament_idx").on(
@@ -39,18 +58,11 @@ export const leaderboardSnapshotTable = pgTable(
 export const leaderboardFeedTable = pgTable(
   "leaderboard_feed",
   {
-    id: uuid("id").primaryKey().defaultRandom(),
-    tourCode: varchar("tour_code", { enum: ["P", "R"] }).notNull(),
-    tournamentId: varchar("tournament_id").notNull(),
-    createdAt: timestamp("created_at").notNull().defaultNow(),
-    updatedAt: timestamp("updated_at")
-      .notNull()
-      .defaultNow()
-      .$onUpdate(() => new Date()),
-    deletedAt: timestamp("deleted_at"),
+    ...defaultColumns,
+    ...tournamentIdentifierColumns,
     seq: serial("seq").notNull(),
-    type: varchar("type").notNull(),
-    feedItem: json("feed_item")
+    type: text("type").notNull(),
+    feedItem: jsonb("feed_item")
       .notNull()
       .$type<RoundStatusChangedV1 | TournamentStatusChangedV1>(),
   },
