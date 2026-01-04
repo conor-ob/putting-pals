76fd0751e4054c3403821d4906d6105f925ce82f
conor-ob <conor.w.ob@gmail.com>
2026-01-04 19:16:14 +0000

    tournament stuff

--- packages/pga-tour-schema/src/types.ts
+++ packages/pga-tour-schema/src/types.ts
@@ -1,1 +1,33 @@
+import type {
+  CompleteScheduleQuery,
+  LeaderboardHoleByHoleQuery,
+  LeaderboardV3Query,
+  ScheduleQuery,
+  ScheduleYearsQuery,
+  TournamentsQuery,
+  UpcomingScheduleQuery,
+} from "./generated/graphql";
+
 export * from "./generated/graphql";
+
+export type ApiLeaderboard = NonNullable<LeaderboardV3Query["leaderboardV3"]>;
+
+export type ApiLeaderboardHoleByHole = NonNullable<
+  LeaderboardHoleByHoleQuery["leaderboardHoleByHole"]
+>;
+
+export type ApiSchedule = NonNullable<ScheduleQuery["schedule"]>;
+
+export type ApiScheduleYears = NonNullable<ScheduleYearsQuery["scheduleYears"]>;
+
+export type ApiCompleteSchedule = NonNullable<
+  CompleteScheduleQuery["completeSchedule"]
+>;
+
+export type ApiUpcomingSchedule = NonNullable<
+  UpcomingScheduleQuery["upcomingSchedule"]
+>;
+
+export type ApiTournament = NonNullable<
+  TournamentsQuery["tournaments"]
+>[number];
