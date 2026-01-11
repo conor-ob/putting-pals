9a7c9fa06825b42cfe2352588e507b0717e5e3f3
conor-ob <conor.w.ob@gmail.com>
2026-01-11 01:07:31 +0000

    updates

--- packages/putting-pals-core/src/leaderboard/leaderboard-aggregator.ts
+++ packages/putting-pals-core/src/leaderboard/leaderboard-aggregator.ts
@@ -1,6 +1,9 @@
 import type {
   Competition,
   DomainLeaderboardV3,
+  DomainPuttingPalsPlayerRowV3,
+  DomainPuttingPalsPlayerScoringDataV3,
+  DomainPuttingPalsPlayerV3,
 } from "@putting-pals/putting-pals-schema";
 
 export function aggregateLeaderboard(
@@ -98,21 +101,22 @@ export function aggregateLeaderboard(
       {
         __typename: "PuttingPalsPlayerRowV3" as const,
         id: competitor.id,
+        leaderboardSortOrder: 0,
         player: {
           __typename: "PuttingPalsPlayerV3" as const,
           countryFlag: competitor.countryFlag,
           displayName: competitor.displayName,
           id: competitor.id,
           shortName: competitor.shortName,
-        },
+        } satisfies DomainPuttingPalsPlayerV3,
         scoringData: {
           __typename: "PuttingPalsPlayerScoringDataV3" as const,
           position: competitor.position,
           total: competitor.total,
           totalSort: competitor.totalSort,
-        },
+        } satisfies DomainPuttingPalsPlayerScoringDataV3,
         picks: competitor.picks.map((pick) => pick.player.id),
-      },
+      } satisfies DomainPuttingPalsPlayerRowV3,
       ...competitor.picks.map((pick) => ({
         ...pick,
         id: `${competitor.id}-${pick.id}`,
