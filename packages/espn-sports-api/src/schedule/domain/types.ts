b76855af256b008d0cccdae1f9c2aa9ee505323a
conor-ob <conor.w.ob@gmail.com>
2026-02-11 00:07:29 +0000

    remove old schemas

--- packages/espn-sports-api/src/schedule/domain/types.ts
+++ packages/espn-sports-api/src/schedule/domain/types.ts
@@ -1,12 +1,9 @@
 import type z from "zod";
 import type {
-  ApiEventSchema,
   TourScheduleEventSchema,
   TourScheduleSchema,
   TourScheduleSeasonsSchema,
 } from "./schemas";
-
-export type ApiEvent = z.infer<typeof ApiEventSchema>;
 
 export type TourSchedule = z.infer<typeof TourScheduleSchema>;
 
