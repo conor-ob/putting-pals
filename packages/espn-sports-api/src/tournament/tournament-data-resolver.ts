28a8469f9847c924ba50f98ec5921d9db6d7e7dc
Conor O'Brien <conor.w.ob@gmail.com>
2026-02-23 19:03:01 +0000

    chore: remove custom tournament info (#110)

--- packages/espn-sports-api/src/tournament/tournament-data-resolver.ts
+++
@@ -1,8 +0,0 @@
-import type { Tournament } from "@putting-pals/putting-pals-core";
-import { INDEX, TOURNAMENTS } from "../generated/tournaments";
-
-export function resolve(id: string): Partial<Tournament> | null {
-  const i = INDEX[id as keyof typeof INDEX];
-
-  return i === undefined ? null : (TOURNAMENTS[i] as Partial<Tournament>);
-}
