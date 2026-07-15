9a617267382a48d780448075059627845ba44114
conor-ob <conor.w.ob@gmail.com>
2026-07-15 22:41:56 +0100

    picks

--- packages/db/src/data/2026/theOpenChampionship.ts
+++ packages/db/src/data/2026/theOpenChampionship.ts
@@ -1,7 +1,74 @@
+import {
+  aidanWalsh,
+  aimeeOhanlon,
+  cameronFolens,
+  conorObrien,
+  davidHynes,
+  johnHannaway,
+  niallCronin,
+  phelimOconnor,
+  richByrne,
+} from "../competitors/puttingPalsCompetitors";
+import {
+  aaronRai,
+  adamScott,
+  alexNoren,
+  brianHarman,
+  chrisGotterup,
+  harrisEnglish,
+  joaquinNiemann,
+  justinRose,
+  kurtKitayama,
+  mattFitzpatrick,
+  maxHoma,
+  robertMacIntyre,
+  russellHenley,
+  scottieScheffler,
+  tomKim,
+  tommyFleetwood,
+} from "../players/pgaTourPlayers";
+
 export default {
   tournamentId: "R2026100",
   paddyPowerId: undefined,
   winnerId: undefined,
   runnerUpId: undefined,
-  competitors: [],
+  competitors: [
+    {
+      ...cameronFolens,
+      picks: [mattFitzpatrick, tommyFleetwood, maxHoma], // 14 + 14 + 125 = 153/1
+    },
+    {
+      ...conorObrien,
+      picks: [scottieScheffler, robertMacIntyre, alexNoren], // 8 + 28 + 100 = 136/1
+    },
+    {
+      ...richByrne,
+      picks: [robertMacIntyre, justinRose, harrisEnglish], // 28 + 30 + 75 = 133/1
+    },
+    {
+      ...phelimOconnor,
+      picks: [robertMacIntyre, russellHenley, aaronRai], // 28 + 45 + 60 = 133/1
+    },
+    {
+      ...niallCronin,
+      picks: [mattFitzpatrick, joaquinNiemann, aaronRai], // 14 + 55 + 60 = 129/1
+    },
+    {
+      ...davidHynes,
+      picks: [tommyFleetwood, mattFitzpatrick, adamScott], // 14 + 14 + 100 = 128/1
+    },
+    {
+      ...aimeeOhanlon,
+      picks: [scottieScheffler, chrisGotterup, kurtKitayama], // 8 + 30 + 90 = 128/1
+    },
+    {
+      ...johnHannaway,
+      picks: [robertMacIntyre, tomKim, aaronRai], // 28 + 40 + 60 = 128/1
+    },
+    {
+      ...aidanWalsh,
+      picks: [mattFitzpatrick, justinRose, brianHarman], // 14 + 30 + 80 = 124/1
+    },
+  ],
 };
